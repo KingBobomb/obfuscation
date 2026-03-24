ddbb2c2ea59a16a57895de4c8c296246eadd29e0
Mohamed Awad <Mohamedemadawad00@gmail.com>
2026-03-24 15:01:09 +0000

    added location class

--- location.py
+++ location.py
@@ -1,1 +1,46 @@
+class Location:
+    def __init__(self, name, description, items=None, exits=None, npcs=None, blocked=False, block_reason=""):
+        self.name = name
+        self.description = description
+        self.items = items if items is not None else []
+        self.exits = exits if exits is not None else {}
+        self.npcs = npcs if npcs is not None else []
+        self.blocked = blocked
+        self.block_reason = block_reason
 
+    def get_items(self):
+        return self.items
+
+    def get_exits(self):
+        return self.exits
+
+    def get_npcs(self):
+        return self.npcs
+
+    def is_blocked(self):
+        return self.blocked
+
+    def add_item(self, item):
+        self.items.append(item)
+
+    def remove_item(self, item):
+        if item in self.items:
+            self.items.remove(item)
+            return True
+        return False
+
+    def add_npc(self, npc):
+        self.npcs.append(npc)
+
+    def remove_npc(self, npc):
+        #Remove an NPC from this location. Returns True if removed, False if not found.
+        if npc in self.npcs:
+            self.npcs.remove(npc)
+            return True
+        return False
+
+    def add_exit(self, direction, location):
+        self.exits[direction] = location
+
+    def __str__(self):
+        return f"{self.name}: {self.description}"
