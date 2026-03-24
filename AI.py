46cdd46bf8136579164e429fd03aef0220295356
ConnorV2727 <connorvandrunen29@gmail.com>
2026-03-23 22:27:50 -0400

    Added additional method stubs and comments

--- AI.py
+++ AI.py
@@ -1,3 +1,5 @@
+import random
+
 def getNpcs(loc):
     """Temporary Function to simulate getting the list of NPCs at a location"""
     if loc == "kitchen":
@@ -8,7 +10,7 @@ def getNpcs(loc):
         return["Guest3", "Guest4"]
     
 def getFurniture(loc):
-    """Temporary Function to simulate getting the list of furniture at a location"""
+    """Temporary Function to simulate getting the list of places to search at a location"""
     if loc == "kitchen":
         return ["Cabinet", "Oven", "Pantry"]
     elif loc == "living room":
@@ -24,30 +26,52 @@ def getExits(loc):
         return {"kitchen:": 1, "foyer": 1, "basement":0}
     elif loc == "foyer":
         return {"living room": 1, "outside":1}
+    elif loc == "basement":
+        return{"living room":0}
+    elif loc == "outside":
+        return{"foyer":1}
 
 class AiAgent:
     """Template class for the AI agent."""
-    def __init__(self, baseNpcWeight, baseSearchWeight, baseMoveWeight, startLocation):
+    def __init__(self, baseNpcWeight, baseSearchWeight, baseMoveWeight, startLocation, incriminatingItemsList):
+        # Base weights for decision making tree
         self.NpcWeight = baseNpcWeight
         self.searchWeight = baseSearchWeight
         self.moveWeight = baseMoveWeight
 
+        # AI starting location 
         self.currLoc = startLocation
-        self.incriminatingItem1Loc = None
-        self.incriminatingItem2Loc = None
-        self.incriminatingItem3Loc = None
-
+        # Collect data for each incriminating item passed in
+        for item in incriminatingItemsList:
+            self.incriminatingItemFoundDict[item] = 0
+        
+        # Initialize the Suspicion Meter
+        self.suspicionMeter = 0
        
+        # Initialize a list to keep track of NPCs in the current room of the AI
         self.NpcsInRoom = []
+        # Initialize a list to keep track of the NPCs that the AI has currently spoken to
         self.NpcsSpokenTo = []
 
+        # Initialize a list to keep track of furniture to search in the AI's current room.
         self.furnitureInRoom = []
+        # Initialize a dictionary to keep track of the furniture the AI has searched and the room it's currently in
         self.furnitureSearched = []
 
-        self.unblockedExits = []
-        self.blockedExits = []
+        # Update values with info gathered from the current room
         self.updateRoomKnowledge()
-        
+
+    def talkToNPC(self, chosenNPC):
+        # Get information about incriminating items from the AI
+        item = chosenNPC.getInfoAI()
+        # Check if the NPC has informed the AI of an incriminating item
+        if item is not None:
+            # 
+            item.getLocation()
+        self.updateRoomKnowledge()
+
+    def searchRoom(self, chosenSearchable):
+         pass      
 
     def moveSelf(self, newLoc):
         self.currLoc = newLoc
@@ -66,12 +90,16 @@ class AiAgent:
             if roomExits[i] == 1:
                 if i in self.blockedExits:
                     self.blockedExits.remove(i)
+                    if i in self.incriminatingItemsDict.keys():
+                        self.incriminatingItemsDict[i] = 1
                 if i not in self.unblockedExits:
                     self.unblockedExits.append(i)
             else:
                 self.blockedExits.append(i)
          
 
-
-    
+    def takeTurn(self):
+        # Check if the game should end, if so return true.
+        if self.suspicionMeter >= 100:
+            return True     
          
