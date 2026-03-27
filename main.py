faedc31d3ed87769ad4712d70a5c9f0669dd15e9
ConnorV2727 <connorvandrunen29@gmail.com>
2026-03-27 17:20:14 -0400

    Added missing newlines to main.py

--- main.py
+++ main.py
@@ -5,10 +5,12 @@ It is intended to be run as a script.
 """
 from game import Game
 
+
 def main():
     """Initializes the Game object and triggers the game to start its main loop."""
     game = Game()
     game.start_game()
+
 
 if __name__ == "__main__":
     main()
