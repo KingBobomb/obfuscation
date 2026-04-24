eeb8019b63c9e5a365c73f50729e1ccdc8d52074
ConnorV2727 <connorvandrunen29@gmail.com>
2026-04-24 17:24:54 -0400

    Made small styling adjustments

--- unit_tests.py
+++ unit_tests.py
@@ -2,10 +2,10 @@
 This module is designed to contain the written unit tests for our code
 """
 import unittest
-from player import Player      
-from npc import NPC             
-from location import Location   
-from Item import Item
+from player import Player
+from npc import NPC
+from location import Location
+from item import Item
 from ai import AiAgent
 
 class AiUnitTests (unittest.TestCase):
@@ -19,9 +19,9 @@ class EnvironmentTests(unittest.TestCase):
         l1 = Location("L1", "Room 1")
         l2 = Location("L2", "Room 2")
         l1.add_exit(l2, 0) # 0 means blocked
-        
+
         self.assertTrue(l1.is_blocked(l2), "Exit should be blocked")
-        
+
         l1.set_exit(l2, 1) # 1 means unblocked
         self.assertFalse(l1.is_blocked(l2), "Exit should now be unblocked")
 
@@ -30,13 +30,13 @@ class EnvironmentTests(unittest.TestCase):
         closet = Location("Closet", "A closet")
         hall.add_exit(closet, 0) # blocked
         closet.add_exit(hall, 0) # blocked
-        
+
         key = Item("Key", "Key", hall, required_location=hall, 
                    target_exit=closet, effect_type="unblock")
-        
+
         # Use the item
         key.use(hall)
-        
+
         # Check if the location state changed
         self.assertFalse(hall.is_blocked(closet))
         self.assertFalse(closet.is_blocked(hall))
